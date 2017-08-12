5d49cc5cab5e71b86810e5f1f2a317e7ac1241f1
gjcxyo <gjcxyo@gmail.com>
2017-08-12 20:52:49 +0900

    Add config.fish

---
+++ fish/config.fish
@@ -0,0 +1,46 @@
+##################################################
+# git
+
+set git_dirty_color red
+set git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_diff (git diff)
+
+  if test -n "$git_diff"
+    echo (set_color $git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+##################################################
+# prompt
+
+function fish_prompt
+  if [ $status -eq 0 ]
+    set status_face (set_color green)"(*'-') > "
+  else
+    set status_face (set_color blue)"(*;-;) > "
+  end
+
+  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+#  set prompt (set_color yellow)(prompt_pwd)
+  set prompt (printf '[%s] %s%s%s@%s%s%s: %s%s%s' (date "+%Y-%m-%d %H:%M:%S") (set_color yellow) (whoami) (set_color normal) (set_color cyan) (hostname|cut -d . -f 1) (set_color normal) (set_color $fish_color_cwd) (pwd) (set_color normal))
+
+#  set date (date "+%Y-%m-%d %H:%M:%S")
+#  set username (whoami)
+#  set hostname (hostname|cut -d . -f 1)
+#  set prompt (set_color $fish_color_cwd) (prompt_pwd)
+
+if test -n "$git_dir"
+#    echo "["$date"] "$username"@"$hostname": "$prompt "("(parse_git_branch)")"
+    echo $prompt "("(parse_git_branch)")"
+    echo $status_face
+  else
+    echo $prompt
+    echo $status_face
+  end
+end
+
